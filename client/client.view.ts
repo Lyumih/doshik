7e694209f35167fd1017b056f59a6b7916c9cae7
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-25 01:54:38 +0300

    fix localStorage zen

--- client/client.view.ts
+++ client/client.view.ts
@@ -3,7 +3,12 @@ namespace $.$$ {
 
 		change_zen_mod() {
 			this.zen(!this.zen())
+      $mol_state_local.value('zen', this.zen())
 		}
+
+    zen(next?: boolean) {
+			return $mol_state_local.value( this.state_key( 'zen' ) , next ) || false
+    }
 
 		sub(): readonly any[] {
 			return [this.zen() ? this.Zen() : this.Full()]
