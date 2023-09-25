4c6e84d08a648f06610df66527bd3da173c191a7
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-25 17:40:57 +0300

    delete zen / add admin

--- client/client.view.ts
+++ client/client.view.ts
@@ -1,18 +1,4 @@
 namespace $.$$ {
 	export class $doshik_client extends $.$doshik_client {
-
-		change_zen_mod() {
-			this.zen( !this.zen() )
-			$mol_state_local.value( 'zen', this.zen() )
-		}
-
-		zen( next?: boolean ) {
-			return $mol_state_local.value( this.state_key( 'zen' ), next ) || false
-		}
-
-		sub(): readonly any[] {
-			return [ this.zen() ? this.Zen() : this.Full() ]
-		}
-
 	}
 }
